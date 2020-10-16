fdbcd6445ec00d3503c4395099f843b0f2675379
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-16 13:32:09 +0300

    localstorage is added

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import Container from "./components/Container/container";
 import "./App.scss";
 
 function App() {
-  const [memes, setMemes] = useState(MEMES_INFO);
-  const [favmemes, setFavMemes] = useState([])
-
+  const [memes, setMemes] = useState(JSON.parse(localStorage.getItem('data'))|| MEMES_INFO);
+  const [favmemes, setFavMemes] = useState((JSON.parse(localStorage.getItem('data'))||[]).filter((item) => item.isLiked));
 
   const setFavorite = (meme) => {
-  let memefavs = [];
-  let handler = [...memes]
-  console.log(meme)
-  handler =handler.map((item) => {
-    console.log(item.id === meme.id )
-     if(item.id === meme.id ){
-       item = { ...item, isLiked: !item.isLiked } 
-     }
+    let memefavs = [];
+    let handler = [...memes];
+    handler = handler.map((item) => {
+      if (item.id === meme.id) {
+        item = { ...item, isLiked: !item.isLiked };
+      }
       return item;
     });
-    memefavs = handler.filter((item)=>item.isLiked)
-    console.log(handler,memefavs)
-    setMemes(handler)
-    setFavMemes(memefavs)
+    memefavs = handler.filter((item) => item.isLiked);
+
+    console.log(2222, handler);
+    console.log(1221, memefavs);
+
+    setMemes(handler);
+    setFavMemes(memefavs);
+    localStorage.setItem("data", JSON.stringify(handler));
+    console.log(localStorage);
   };
   // memes.map((meme) => {
   //   console.log(123, id)
