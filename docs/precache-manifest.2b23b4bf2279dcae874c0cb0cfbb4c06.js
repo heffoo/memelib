fd6bf977259fd75124f0163577942da89fcae8b3
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-22 13:30:53 +0300

    Save commit

---
+++ docs/precache-manifest.2b23b4bf2279dcae874c0cb0cfbb4c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e617bf0e4d2637072c6a19dadbe1a23",
+    "url": "/memelib/index.html"
+  },
+  {
+    "revision": "dde7f1ee05480dcd898a",
+    "url": "/memelib/static/css/main.abe66bad.chunk.css"
+  },
+  {
+    "revision": "add9be79fba2f23dd41e",
+    "url": "/memelib/static/js/2.be53dd8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memelib/static/js/2.be53dd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde7f1ee05480dcd898a",
+    "url": "/memelib/static/js/main.eeef7dd3.chunk.js"
+  },
+  {
+    "revision": "7b42e8b73712097c195e",
+    "url": "/memelib/static/js/runtime-main.7d4d8507.js"
+  }
+]);
